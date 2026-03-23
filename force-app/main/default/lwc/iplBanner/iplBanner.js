9ca901970a04401172bcdf171ae9dc604bb5b11b
Alan McClenaghan <alandmcclenaghan@icloud.com>
2026-03-23 17:11:29 +0000

    Data controller setup

--- force-app/main/default/lwc/iplBanner/iplBanner.js
+++ force-app/main/default/lwc/iplBanner/iplBanner.js
@@ -1,3 +1,16 @@
-import { LightningElement } from 'lwc';
+import { LightningElement, wire } from 'lwc';
+import fetchCricketData from '@salesforce/apex/IplController.fetchCricketData'
+export default class IplBanner extends LightningElement {
 
-export default class IplBanner extends LightningElement {}
+    fileName='ipl2024stats.json'
+    @wire(fetchCricketData, {
+        fileName:'$fileName'
+    })ipl2023StatsHandler({data, error}){
+        if(data){
+            console.log("ipl2023StatsHandler data", data)
+        }
+        if(error){
+            console.error("ipl2023StatsHandler error", error);
+        }
+    }
+}
